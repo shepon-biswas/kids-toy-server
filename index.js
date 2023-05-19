4b73e04047ef2df4963c0a1d41ec475a7bb394ce
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-20 00:35:51 +0600

    env file created & replace mongodb username and password using .env file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
+// midleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) =>{
     res.send("Kids Toys Server is running")
 })
 
-const uri = "mongodb+srv://kidsToys:Hk0cAVb3yIraWDuC@kids-toys.ebcxkyg.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@kids-toys.ebcxkyg.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
