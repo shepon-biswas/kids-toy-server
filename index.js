426d90b6ca7dda3936ef65d648e9923102e716ad
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-21 20:56:51 +0600

    Final Deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const toysCollection = client.db("toysDB").collection("toys");
     // create index on for data searching option
     const indexKeys = {name: 1, subCategory: 1};
